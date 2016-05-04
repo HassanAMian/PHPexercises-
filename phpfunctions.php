c99cb85dc9eec921be81d160465beab118263365
HassanAMian <hassanmian83@gmail.com>
2016-05-03 22:44:28 -0400

    finished function 2

--- phpfunctions.php
+++ phpfunctions.php
@@ -6,6 +6,22 @@
 
 
 2. Write a function to check a number is prime or not. 
+<?php
+	function Prime($num) {
+    	if($num == 1)
+        	return false;
+    	if($num == 2)
+        	return true;
+    	if($num % 2 == 0) {
+        	return false;
+    	}
+    	for($i = 3; $i <= ceil(sqrt($num)); $i = $i + 2) {
+        	if($num % $i == 0)
+            	return false;
+    	}
+    	return true;
+	}
+?>
 
 
 3. Write a function to reverse a string. 

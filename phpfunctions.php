23cdda52f1223239038d8c8b226bc82f7bd13bcb
HassanAMian <hassanmian83@gmail.com>
2016-05-05 22:47:01 -0400

    completed exercise 2 of php functions

--- phpfunctions.php
+++ phpfunctions.php
@@ -25,6 +25,22 @@
 
 
 3. Write a function to reverse a string. 
+<?php  
+function reverse($str1)  
+{  
+ $n = strlen($str1);  
+ if($n == 1)  
+   {  
+    return $str1;  
+   }  
+ else  
+   {  
+   $n--;  
+   return reverse(substr($str1,1, $n)) . substr($str1, 0, 1);  
+   }  
+}  
+print_r(reverse('hello world!'));  
+?>  
 
 
 4. Write a function to sort an array. 

ce00a903df0087d541a712ba7b4d0d3c9660f2fe
HassanAMian <hassanmian83@gmail.com>
2016-05-06 23:30:06 -0400

    completed some more php exercises (array)

--- phparrays.php
+++ phparrays.php
@@ -6,8 +6,10 @@
 Write a script which will display the following string - 
 "The memory of that scene for me is like a frame of film forever frozen at that moment: the red carpet, the green lawn, the white house, the leaden sky. The new president and his first lady. - Richard M. Nixon"
 and the words 'red', 'green' and 'white' will come from $color.
-
-
+<?php  
+	$color = array('white', 'green', 'red', 'blue', 'black');  
+	echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the $color[2] carpet, the $color[1] lawn, the $color[0] house, the leaden sky. The new president and his first lady. - Richard M. Nixon";  
+?>  
 
 2. $color = array('white', 'green', 'red'') 
 Write a PHP script which will display the colors in the following way : 

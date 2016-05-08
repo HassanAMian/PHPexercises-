913cf323890551f177cd37d74cd3bb4889d7945f
HassanAMian <hassanmian83@gmail.com>
2016-05-07 22:32:37 -0400

    finished more array problems

--- phparrays.php
+++ phparrays.php
@@ -19,7 +19,20 @@ white, green, red,
 green
 red
 white
-
+<?php  
+	$color = array('white', 'green', 'red');  
+	foreach ($color as $i)  
+	{  
+		echo "$i, ";  
+	}  
+	sort($color);  
+	echo "<ul>";  
+	foreach ($color as $a)  
+	{  
+		echo "<li>$a</li>";  
+	}  
+	echo "</ul>";  
+?> 
 
 3. $ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
 
